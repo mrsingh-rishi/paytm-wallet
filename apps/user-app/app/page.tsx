32c0f5549d938d0e6448cc9476aa16d3e8d00d7f
Rishi Singh <rs3949427@gmail.com>
2024-04-14 00:12:07 +0530

    added prisma to the common packages

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,7 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
+
 export default function Page(): JSX.Element {
-  return (
-    <div className="text-2xl">
-      hello there!
-    </div>
-  );
+  return <div className="text-2xl">hello there!</div>;
 }
