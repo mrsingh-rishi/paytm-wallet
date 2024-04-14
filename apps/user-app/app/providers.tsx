aa3cd6dd6bed3246adbf21cd2c703eb0abb0e393
Rishi Singh <rs3949427@gmail.com>
2024-04-14 12:28:05 +0530

    added appbar component

--- apps/user-app/app/providers.tsx
+++ apps/user-app/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <RecoilRoot>
+      <SessionProvider>{children}</SessionProvider>
+    </RecoilRoot>
+  );
 };
