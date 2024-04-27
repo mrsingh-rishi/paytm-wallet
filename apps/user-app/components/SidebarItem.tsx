ed514addf5d40e0595eff114bd6c90749d412836
Rishi Singh <rs3949427@gmail.com>
2024-04-27 12:49:22 +0530

    UI fix

--- apps/user-app/components/SidebarItem.tsx
+++ apps/user-app/components/SidebarItem.tsx
@@ -17,14 +17,14 @@ export const SidebarItem = ({
 
   return (
     <div
-      className={`flex ${selected ? "text-[#6a51a6]" : "text-slate-500"} cursor-pointer p-2 pl-8`}
+      className={`flex ${selected ? "text-blue-400" : "text-slate-500"} cursor-pointer p-2 pl-8`}
       onClick={() => {
         router.push(href);
       }}
     >
       <div className="pr-2">{icon}</div>
       <div
-        className={`font-bold ${selected ? "text-[#6a51a6]" : "text-slate-500"}`}
+        className={`font-bold ${selected ? "text-blue-400" : "text-slate-500"}`}
       >
         {title}
       </div>
